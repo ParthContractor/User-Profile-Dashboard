c64f7266e9385b9f56407b0849219fddbc9def15
ParthContractor <contractor110@gmail.com>
2025-07-15 18:08:14 +1000

    Initial Commit

---
+++ UserProfileDashboard/UserProfileDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserProfileDashboardApp.swift
+//  UserProfileDashboard
+//
+//  Created by Parth Contractor on 15/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct UserProfileDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UserProfileDashboardTests/UserProfileDashboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserProfileDashboardTests.swift
+//  UserProfileDashboardTests
+//
+//  Created by Parth Contractor on 15/7/2025.
+//
+
+import Testing
+@testable import UserProfileDashboard
+
+struct UserProfileDashboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
